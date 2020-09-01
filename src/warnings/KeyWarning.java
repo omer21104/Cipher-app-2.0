2d1983ef58e9fe8ea2f99342d4e000f2199983d1
Omer <Omer21104@gmail.com>
2020-09-01 13:03:56 +0300

    #3: Added encryption / decryption functionality

---
+++ src/warnings/KeyWarning.java
@@ -0,0 +1,12 @@
+package warnings;
+
+import javax.swing.JOptionPane;
+
+public class KeyWarning {
+	
+	public KeyWarning()
+	{
+		JOptionPane.showMessageDialog(null, "Key can contain positive numbers only!");
+	}
+
+}
